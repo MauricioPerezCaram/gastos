c4d82531438692931d60ec0fef7ef194ab6d8a78
Mauricio Pérez <perezcarammauricio@hotmail.com>
2024-07-04 17:05:12 -0300

    fin cookies

--- server/src/routers/api/cookies.router.js
+++ server/src/routers/api/cookies.router.js
@@ -2,17 +2,49 @@ import { Router } from "express";
 
 const cookiesRouter = Router();
 
-cookiesRouter.get("/set/:mode", async (req, res, next) => {
+cookiesRouter.get("/set/:modo", async (req, res, next) => {
   try {
-    const { mode } = req.params;
-    const maxAge = 10000;
+    const { modo } = req.params;
+    const maxAge = 60000 * 5;
     const signed = true;
     return res
-      .cookie("mode", mode, { maxAge })
+      .cookie("modo", modo, { maxAge })
       .cookie("sessionId", "hola1234", { maxAge, signed })
       .json({
         statusCode: 200,
-        message: "Cookie configurada - Modo: " + mode,
+        message: "Cookie configurada - Modo: " + modo,
+      });
+  } catch (error) {
+    return next(error);
+  }
+});
+
+cookiesRouter.get("/get", async (req, res, next) => {
+  try {
+    const modo = req.cookies.modo;
+    const sessionId = req.signedCookies.sessionId;
+    return res.json({
+      response: {
+        modo,
+        sessionId,
+      },
+    });
+  } catch (error) {
+    return next(error);
+  }
+});
+
+cookiesRouter.get("/clear", async (req, res, next) => {
+  try {
+    return res
+      .clearCookie("modo")
+      .clearCookie("sessionId")
+      .json({
+        statusCode: 200,
+        response: {
+          modo: req.cookies.modo,
+          sessionId: req.signedCookies.sessionId,
+        },
       });
   } catch (error) {
     return next(error);
